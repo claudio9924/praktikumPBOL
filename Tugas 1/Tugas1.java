f174fcbc144ccce98b9e0dad724336d42c6a174e
claudio9924 <151592125+claudio9924@users.noreply.github.com>
2024-09-19 03:02:52 +0700

    Add files via upload

---
+++ Tugas 1/Tugas1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan Nama : ");
+        String nama = scanner.nextLine();
+        System.out.print("Masukkan NIM : ");
+        int nim = scanner.nextInt();
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        scanner.close();
+    }
+}
